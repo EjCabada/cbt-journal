acd52d4513ba8e015d2b628e3bc4f8f5aa8ba957
EJ Cabada <cabada.eric7@gmail.com>
2025-01-24 16:49:04 -0800

    deleted app.jsx, added css elements, designed home.jsx

--- cbt-journal/src/Header.jsx
+++ cbt-journal/src/Header.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div id='header'>
+      <h1>CBT Journal</h1>
+      <nav>
+        <span className="navLinks">Home</span>
+        <span className="navLinks">About</span>
+        <span className="navLinks">Journal Entries Catalog</span>
+        <span className="navLinks">Other Projects</span>
+      </nav>
+    </div>
   )
 }
 
--- cbt-journal/src/Home.jsx
+++ cbt-journal/src/Home.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h2>Welcome to the Cognitive Behavioral Therapy Journal</h2>
+      <h3>We are glad you are here</h3>
+      <div>This is a safe space for you to journal and relfect on your personal circumstances and journey.</div>
+      <h3>Your information is safe</h3>
+      <div>As creators of the CBT Journal, we have a firm conviction that your information and personal experiences are private and belong to you alone.</div>
+      <div>As such our app does not collect any infomration what so ever. All your journal entries are saved locally in your browser so only you have access to that information</div>
+      <div><strong>Happy Journaling!</strong>
+      </div>
+    </div>
   )
 }
 
